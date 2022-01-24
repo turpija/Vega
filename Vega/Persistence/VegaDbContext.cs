a8b9fc29b66539865b3c031c63bd3a00c0cdd859
Ivan Kočiš <turpija@gmail.com>
2022-01-24 15:57:59 +0100

    initialMigration

---
+++ Vega/Persistence/VegaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vega.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
